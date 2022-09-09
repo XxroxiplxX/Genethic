11de5014ca2d1633d00109a83c0c94d51b658946
piotrkali <261696@student.pwr.edu.pl>
2022-09-09 03:55:28 -0400

    init with some tests

--- tst/Individual-test.cpp
+++ tst/Individual-test.cpp
@@ -1,3 +1,19 @@
 //
 // Created by piotrkali on 9/9/22.
 //
+#include "gtest/gtest.h"
+#include "genethic-lib/Individual.h"
+
+TEST(IndTest, genome_corectness) {
+    //arrange
+    //act
+    //assert
+    int *data = new int[10];
+    for (int i = 0; i < 10; i++) {
+        data[i] = i;
+    }
+    Individual individual = Individual(10, data);
+    individual.print_individual();
+    EXPECT_EQ (individual.get_gen(3),  3);
+
+}
--- tst/Population-test.cpp
+++ tst/Population-test.cpp
@@ -1,3 +1,20 @@
 //
 // Created by piotrkali on 9/9/22.
 //
+#include "gtest/gtest.h"
+#include "genethic-lib/Individual.h"
+#include "genethic-lib/Population.h"
+
+TEST(Poptest, populationgen) {
+    int k = 100;
+    int **distances = nullptr;
+    int *data = new int[k];
+    int *data2 = new int[k];
+    for (int i = 0; i < k; i++) {
+        data[i] = i;
+        data2[i] = k - i;
+    }
+    Individual second = Individual(k, data2);
+    Individual pioneer = Individual(k, data);
+    Population population = Population(pioneer, distances, k, 0.8);
+}
